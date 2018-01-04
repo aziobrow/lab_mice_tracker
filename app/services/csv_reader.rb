f09389c8aef10375852519aa0a5a8a816ab72766
Aurora Ziobrowski <aziobrow@gmail.com>
2018-01-03 18:54:43 -0700

    finished poro methods to clean data

--- app/services/csv_reader.rb
+++ app/services/csv_reader.rb
@@ -8,12 +8,71 @@ class CSVReader
     @csv_file = csv_file
   end
 
-  def to_boolean(value)
+  def clean_trisomic(value)
     trisomic_value = value.downcase
     if trisomic_value == "yes"
       return true
     elsif trisomic_value == "no"
       return false
+    else
+      return nil
+    end
+  end
+
+  def is_float?(value)
+    true if Float(value) rescue false
+  end
+
+  def clean_protein(value)
+    if is_float?(value)
+      return value.to_f
+    else
+      return nil
+    end
+  end
+
+  def clean_diet(value)
+    diet_value = value.downcase
+    if diet_value == 'rapa' || diet_value == "rapamycin"
+      return "rapamycin"
+    elsif diet_value == 'cont' || diet_value == "control"
+      return "control"
+    else
+      return nil
+    end
+  end
+
+  def clean_color(value)
+    color_value = value.downcase
+    if color_value == 'black' || color_value == 'agouti'
+      return color_value
+    else
+      return nil
+    end
+  end
+
+  def clean_gender(value)
+    gender_value = value.downcase
+    if gender_value == 'male' || gender_value == 'female'
+      return gender_value
+    else
+      return nil
+    end
+  end
+
+  def clean_brain_temp(value)
+    if value == "CD"
+      return value
+    elsif is_float?(value)
+      return value.to_f
+    else
+      return nil
+    end
+  end
+
+  def clean_weight(value)
+    if is_float?(value)
+      return value.to_f
     else
       return nil
     end
--- spec/services/csv_reader_spec.rb
+++ spec/services/csv_reader_spec.rb
@@ -7,12 +7,63 @@ describe CSVReader do
     expect(reader.csv_file).to eq('/data/mouse_attributes.csv')
   end
 
-  it "converts string value to a boolean" do
-    expect(reader.to_boolean("Yes")).to eq(true)
-    expect(reader.to_boolean("yes")).to eq(true)
-    expect(reader.to_boolean("False")).to eq(false)
-    expect(reader.to_boolean("false")).to eq(false)
-    expect(reader.to_boolean("xyz")).to eq(nil)
+  it "converts trisomic value to a boolean" do
+    expect(reader.clean_trisomic("Yes")).to eq(true)
+    expect(reader.clean_trisomic("yes")).to eq(true)
+    expect(reader.clean_trisomic("No")).to eq(false)
+    expect(reader.clean_trisomic("no")).to eq(false)
+    expect(reader.clean_trisomic("xyz")).to eq(nil)
+    expect(reader.clean_trisomic("")).to eq(nil)
+  end
+
+  it "identifies valid floats" do
+    expect(reader.is_float?("1")).to eq(true)
+    expect(reader.is_float?("54.842")).to eq(true)
+    expect(reader.is_float?("")).to eq(false)
+    expect(reader.is_float?("xyz")).to eq(false)
+  end
+
+  it "converts protein value to a float" do
+    expect(reader.clean_protein("")).to eq(nil)
+    expect(reader.clean_protein("xyz")).to eq(nil)
+    expect(reader.clean_protein("2.6145")).to eq(2.6145)
+  end
+
+  it "converts diet value" do
+    expect(reader.clean_diet("")).to eq(nil)
+    expect(reader.clean_diet("xyz")).to eq(nil)
+    expect(reader.clean_diet("Rapamycin")).to eq("rapamycin")
+    expect(reader.clean_diet("Rapa")).to eq("rapamycin")
+    expect(reader.clean_diet("Cont")).to eq("control")
+    expect(reader.clean_diet("Control")).to eq("control")
+  end
+
+  it "converts color value" do
+    expect(reader.clean_color("")).to eq(nil)
+    expect(reader.clean_color("xyz")).to eq(nil)
+    expect(reader.clean_color("black")).to eq("black")
+    expect(reader.clean_color("agouti")).to eq("agouti")
+  end
+
+  it "converts gender value" do
+    expect(reader.clean_gender("")).to eq(nil)
+    expect(reader.clean_gender("xyz")).to eq(nil)
+    expect(reader.clean_gender("Male")).to eq("male")
+    expect(reader.clean_gender("Female")).to eq("female")
+  end
+
+  it "converts brain temp to float" do
+    expect(reader.clean_brain_temp("")).to eq(nil)
+    expect(reader.clean_brain_temp("xyz")).to eq(nil)
+    expect(reader.clean_brain_temp("CD")).to eq("CD")
+    expect(reader.clean_brain_temp("87.9")).to eq(87.9)
+  end
+
+  it "converts weight to float" do
+    expect(reader.clean_brain_temp("")).to eq(nil)
+    expect(reader.clean_brain_temp("n/a")).to eq(nil)
+    expect(reader.clean_brain_temp("39.8")).to eq(39.8)
+    expect(reader.clean_brain_temp("41.0")).to eq(41.0)
   end
 
 end
